b63a71e96ddd54ba4faf600b381cfb898d6b529d
laho0011 <quentin.lahousse@etudiant.univ-reims.fr>
2025-07-25 18:46:49 +0200

    Calendar.jsx, useCalendarList.js : perfectionnement du style avec l'ajout du nombre de mois et des bonnes couleurs

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -1,21 +1,58 @@
-import { useEffect, useState } from "react";
+"use client"
+import {useRef, useEffect, useState, cloneElement} from "react";
 import useCalendarList from "../hooks/useCalendarList";
+import useGsapInertiaHover from "../hooks/useGsapInertiaHover";
+import useGsapSplitText from "@/hooks/useGsapSplitText";
 
 export default function Calendar({ age }) {
-    const {calendarList, makeList} = useCalendarList();
-    console.log(age)
+    const containerRef = useRef(null);
+    const { calendarList, makeList } = useCalendarList();
+
+    const [activityTotals, setActivityTotals] = useState(null)
+
     useEffect(() => {
-        makeList(age)
+        setActivityTotals(makeList(age));
     }, [age]);
 
+    useGsapInertiaHover(containerRef, [calendarList]);
+    useGsapSplitText(".split");
+
+    console.log(activityTotals)
+
     return (
-        <div className="grid grid-flow-row-dense grid-cols-36 grid-rows-3 gap-2">
-            {calendarList.map((block, key) => (
-                <div
-                    key={key}
-                    className={` ${block.className} rounded-sm h-5 w-5`}
-                />
-            ))}
+        <div>
+            {activityTotals ? (
+                <h2
+                    className="split z-10a text-justify text-2xl font-semibold text-gray-800 mb-8">
+                    Au long de votre vie vous avez passé
+                    {" "+activityTotals.sleep} mois à
+                    <span className="split  text-sky-600! font-mono font-bold duration-300 hover:text-3xl"> dormir</span>, {" "+activityTotals.work} à
+                    <span className="split text-amber-700! font-mono font-bold duration-300 hover:text-3xl"> travailler</span>, {" "+activityTotals.domesticWork} aux
+                    <span className="split text-lime-600! font-mono font-bold duration-300 hover:text-3xl"> travaux ménager</span>, {" "+activityTotals.eating} à
+                    <span className="split text-orange-600! font-mono font-bold duration-300 hover:text-3xl"> manger</span>, {" "+activityTotals.transport} en
+                    <span className="split text-emerald-600! font-mono font-bold duration-300 hover:text-3xl"> transports</span>, {" "+activityTotals.hygiene} pour votre
+                    <span className="split text-indigo-600! font-mono font-bold duration-300 hover:text-3xl"> hygiène </span>et {" "+activityTotals.activities} pour vos
+                    <span className="split text-purple-600! font-mono font-bold duration-300 hover:text-3xl"> loisirs</span>
+                </h2>
+            ) :
+                (
+                    <></>
+                )
+            }
+
+            <div
+                ref={containerRef}
+                className="mwg_effect000 grid grid-flow-row-dense grid-cols-36 grid-rows-3 gap-1"
+            >
+                {calendarList.map((block, key) => (
+                    <div
+                        key={key}
+                        className={`media ${block.className} rounded-sm h-5 w-5`}
+                        id={block.id}
+                    />
+                ))}
+            </div>
         </div>
+
     );
 }
--- src/hooks/useCalendarList.js
+++ src/hooks/useCalendarList.js
@@ -1,6 +1,6 @@
-import {useState} from "react";
+import { useState } from "react";
 
-export default function useCalendarList () {
+export default function useCalendarList() {
     const [calendarList, setCalendarList] = useState([]);
 
     const makeList = (age) => {
@@ -24,17 +24,28 @@ export default function useCalendarList () {
         const monthLeft = Math.max(0, totalMonths - totalUsed);
 
         const list = [
-            ...Array(sleep).fill({ className: 'bg-emerald-800', id: 'sleep' }),
-            ...Array(work).fill({ className: 'bg-blue-800', id: 'work' }),
-            ...Array(domesticWork).fill({ className: 'bg-yellow-800', id: 'domesticWork' }),
-            ...Array(eating).fill({ className: 'bg-pink-800', id: 'eating' }),
-            ...Array(activities).fill({ className: 'bg-purple-800', id: 'activities' }),
-            ...Array(transport).fill({ className: 'bg-orange-800', id: 'transport' }),
-            ...Array(hygiene).fill({ className: 'bg-gray-800', id: 'hygiene' }),
-            ...Array(monthLeft).fill({ className: 'border-3 border-indigo-600/40', id: 'free' }),
+            ...Array(sleep).fill({ className: 'bg-sky-600', id: 'sleep' }),
+            ...Array(work).fill({ className: 'bg-amber-700', id: 'work' }),
+            ...Array(domesticWork).fill({ className: 'bg-lime-600', id: 'domesticWork' }),
+            ...Array(eating).fill({ className: 'bg-orange-600', id: 'eating' }),
+            ...Array(transport).fill({ className: 'bg-emerald-600', id: 'transport' }),
+            ...Array(hygiene).fill({ className: 'bg-indigo-600', id: 'hygiene' }),
+            ...Array(activities).fill({ className: 'bg-purple-600', id: 'activities' }),
+            ...Array(monthLeft).fill({ className: 'border-2 border-slate-900/50', id: 'free' }),
         ];
 
         setCalendarList(list);
-    }
-    return {makeList,calendarList}
+
+        return {
+            sleep,
+            work,
+            domesticWork,
+            eating,
+            activities,
+            transport,
+            hygiene,
+        };
+    };
+
+    return { makeList, calendarList };
 }
